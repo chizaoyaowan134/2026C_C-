284ae15d55945bf4a863f32e53ae0aaf77fe9b08
chizaoyaowan <1498431730@qq.com>
2026-01-31 14:18:57 +0800

    day19

---
+++ day19/code_4.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 3) {
+        for (int i = 0; i < n; i ++) {
+
+            for (int j = 0; j < i; j ++) {
+                printf("*");
+            }
+            printf("\n");
+        }
+
+    }
+
+
+    return 0;
+
+
+}
